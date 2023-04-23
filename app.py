2bbbfff3c00524748d2ff0a9360ce0ffe8e18312
a.lazev@transdata.no <a.lazev@transdata.no>
2023-04-24 01:05:35 +0300

    PEP8

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from service import ScrapeService
-from pathlib import Path
 import pandas as pd
-from app_config import basedir
 
 
 if __name__ == '__main__':
     service = ScrapeService()
     movies = service.get_top_movies()
     df = pd.DataFrame.from_dict(movies)
-    df.to_csv(Path(basedir) / 'top_movies.csv')
+    # df.to_csv(Path(basedir) / 'top_movies.csv')
